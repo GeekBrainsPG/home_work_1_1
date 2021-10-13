4a0a7a24d6b891ad9b2c249282ca80040e4860f0
Petr GUlchuk <petr.gulchuk@gmail.com>
2021-10-13 03:36:54 -0400

    created method printThreeWords

--- src/main/java/com/geekbrains/home_work_1/HomeWorkApp.java
+++ src/main/java/com/geekbrains/home_work_1/HomeWorkApp.java
@@ -3,7 +3,13 @@ package com.geekbrains.home_work_1;
 public class HomeWorkApp {
 
     public static void main(String[] args) {
-        System.out.println("Hi there");
+        printThreeWords();
+    }
+
+    private static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
     }
 
 }
