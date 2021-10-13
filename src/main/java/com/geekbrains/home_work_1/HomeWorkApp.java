7db8b848710fce898a3b610a9dfeaed478af79f0
Petr GUlchuk <petr.gulchuk@gmail.com>
2021-10-13 03:40:05 -0400

    created method checkSumSign

--- src/main/java/com/geekbrains/home_work_1/HomeWorkApp.java
+++ src/main/java/com/geekbrains/home_work_1/HomeWorkApp.java
@@ -4,12 +4,25 @@ public class HomeWorkApp {
 
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
 
     private static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+
+    private static void checkSumSign() {
+        int a = 42;
+        int b = 24;
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("\nСумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
     }
 
 }
